4a76a45a539edb092e0c0044552c3c0672d3adfe
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2021-08-11 13:10:38 -0300

    14-react-router

---
+++ 14-react-router/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Home from './components/Home';
+import User from './components/User';
+
+const App = () => {
+
+    return (
+        <h1>React Router</h1>
+    )
+}
+
+export default App;
