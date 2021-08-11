81ef032fc2dca40fbb2861ed21a4f0e55edfd973
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2021-08-11 13:27:26 -0300

    props strict & sensitive

--- 14-react-router/src/App.js
+++ 14-react-router/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import User from './components/User';
@@ -6,7 +8,12 @@ import User from './components/User';
 const App = () => {
 
     return (
-        <h1>React Router</h1>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/user" component={User} />
+        {/* <Route path="/User" sensitive component={User} /> */}
+        {/* <Route path="/user/" strict component={User} /> */}
+      </Router>
     )
 }
 
