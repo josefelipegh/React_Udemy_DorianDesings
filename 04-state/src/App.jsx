52d9dce0bfa8a548ecd4050f065eddbff0d3ba3d
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2022-01-06 17:47:30 -0300

    Estructura de un class component

--- 04-state/src/App.jsx
+++ 04-state/src/App.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { otherNumber: 5 };
+  // }
+
+  // state = { number: 5 }
+
   render() {
+    const {number, name} = this.props
     return (
-      <h1>Class component</h1>
+      <h1>{name} {number}</h1>
     );
   }
 }
--- 04-state/src/index.js
+++ 04-state/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App number={6} name='Felipe' />
   </React.StrictMode>,
   document.getElementById('root')
 );
