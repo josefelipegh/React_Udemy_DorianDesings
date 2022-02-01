5f4e723dbae0880ee4f76edb1aee24186225b484
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2022-02-01 01:22:45 -0300

    Controlando radio button con state

--- 08-forms/src/components/Form.jsx
+++ 08-forms/src/components/Form.jsx
@@ -4,7 +4,8 @@ import './form.css'
 class Form extends Component {
     
     state = {
-        name: ''
+        name: '',
+        gender: ''
     }
 
     handleSubmit = (e) => {
@@ -13,7 +14,10 @@ class Form extends Component {
     }
 
     handleChange = (e) => {
-        this.setState({name: e.target.value})
+        console.log(e.target.name)
+        this.setState({
+            [e.target.name] : e.target.value
+        })
     }
 
     render() {
@@ -25,20 +29,42 @@ class Form extends Component {
                         <label className="form__label" htmlFor="name">Name</label>
                         <input 
                             className="form__input" 
-                            id="name" 
+                            id="name"
+                            name='name' 
                             onChange={this.handleChange}
                             type="text" 
                         />
                     </div>
-                    {/* <div className="form__field">
+                    <div className="form__field">
                         <label className="form__label form__label--radio" htmlFor="man">Man</label>
-                        <input className="form__input" type="radio" id="man" name="gender" />
+                        <input 
+                                className="form__input" 
+                                id="man" 
+                                name="gender"
+                                onChange={this.handleChange} 
+                                type="radio" 
+                                value='man'
+                        />
 
                         <label className="form__label form__label--radio" htmlFor="woman">Woman</label>
-                        <input className="form__input" type="radio" id="woman" name="gender" />
+                        <input 
+                                className="form__input" 
+                                id="woman" 
+                                name="gender"
+                                onChange={this.handleChange} 
+                                type="radio" 
+                                value='woman'
+                        />
 
                         <label className="form__label form__label--radio" htmlFor="other">Other</label>
-                        <input className="form__input" type="radio" id="other" name="gender" />
+                        <input 
+                                className="form__input" 
+                                id="other" 
+                                name="gender"
+                                onChange={this.handleChange} 
+                                type="radio" 
+                                value='other'
+                        />
                     </div>
                     <div className="form__field">
                         <label className="form__label" htmlFor="languaje">Language</label>
@@ -50,7 +76,7 @@ class Form extends Component {
                     <div className="form__field">
                         <label className="form__label" htmlFor="terms">Terms and conditions</label>
                         <input className="form__input" type="checkbox" id="other" name="gender" />
-                    </div> */}
+                    </div>
                     <input className="form__submit" type="submit" value="send" />
                     <p>{JSON.stringify(this.state)}</p>
                 </form >
