b3b0864140709fa87042e1451ddce0835f5f8f17
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2022-03-09 17:04:43 -0300

    Creando nuestro primer Styled Component

--- 10-styled-components/src/components/button/Button.jsx
+++ 10-styled-components/src/components/button/Button.jsx
@@ -1,5 +1,15 @@
-import './button.css';
+import styled from 'styled-components';
 
-const Button = () => <button className="button">Click me!</button>
+const StyledButton = styled.button`
+    background-color: royalblue;
+    color: #fff;
+
+    &:hover {
+        background-color : red;
+        color: royalblue;
+    }
+`
+
+const Button = () => <StyledButton>Click me!</StyledButton>
 
 export default Button;
