b5406fec93b15e4254006d84873412c99361db83
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2022-03-10 11:49:34 -0300

    Responsive

--- 10-styled-components/src/App.jsx
+++ 10-styled-components/src/App.jsx
@@ -2,11 +2,12 @@ import Button from "./components/button/Button";
 import Title from "./components/title/Title";
 
 import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
-import colors from "./components/theming/colors";
+import colors from "./theming/colors";
+import Responsive from "./components/responsive/Responsive";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background-color: greenyellow;
+    background-color: goldenrod;
   }
 `
 
@@ -21,11 +22,14 @@ function App() {
       <GlobalStyle/>
       <Title/>
       <ThemeProvider theme={colors}>
-        <Button>Click Me!</Button>
-        <Button danger>Click Me!</Button>
-        <Button ghost>Ghost Me!</Button>
-        <Button danger ghost>Ghost Me!</Button>
-        <ButtonRounded danger ghost>Rounded</ButtonRounded>
+        <Responsive>
+            <Button>Click Me!</Button>
+            <Button danger>Click Me!</Button>
+            <Button ghost>Ghost Me!</Button>
+            <Button danger ghost>Ghost Me!</Button>
+            <ButtonRounded danger ghost>Rounded</ButtonRounded>
+            <Button>Click Me!</Button>
+        </Responsive>
       </ThemeProvider>
     </>
   );
