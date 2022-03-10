f66004848fd1bf1591f08b14c51caa734b1c9043
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2022-03-09 21:26:34 -0300

    Anidamiento

--- 10-styled-components/src/components/title/Title.jsx
+++ 10-styled-components/src/components/title/Title.jsx
@@ -6,17 +6,37 @@ const getBorder = (width, type, color) => {
     return `${width} ${type} ${color}`
 }
 
-const TitleStyled = styled.h1`
+const Header = styled.header`
     text-align: center; 
     border: ${getBorder('3px', 'solid', 'royalblue')};
+    margin-bottom: 1rem;
+
+    & h1 {
+        margin: 0;
+        color: red;
+    }
+
+    & h1::after {
+        content: '';
+        display: block;
+        width: 100px;
+        height: 100px;
+        background-color: greenyellow;
+    }
+
+    &:hover ::after {
+        background-color: blue;
+    }
 `
 
 const Title = () => {
 
     return (
-        <>
-            <TitleStyled>Styled Components</TitleStyled>
-        </>
+        <Header>
+            <h1>
+                Styled Components
+            </h1>
+        </Header>
     );
 }
 
