ae1080385eb616467fdb1625a14b8aa2735d44b6
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2022-03-09 21:31:01 -0300

    Estilos Globales

--- 10-styled-components/src/App.js
+++ 10-styled-components/src/App.js
@@ -1,12 +1,19 @@
 import Button from "./components/button/Button";
 import Title from "./components/title/Title";
 
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import colors from "./components/theming/colors";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: greenyellow;
+  }
+`
 
 function App() {
   return (
     <>
+      <GlobalStyle/>
       <Title/>
       <ThemeProvider theme={colors}>
         <Button>Click Me!</Button>
