8f9e347f5776620c5b475c773eb8cb8da38493ba
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2022-03-09 21:47:29 -0300

    Heredando estilos

--- 10-styled-components/src/App.jsx
+++ 10-styled-components/src/App.jsx
@@ -1,13 +1,18 @@
 import Button from "./components/button/Button";
 import Title from "./components/title/Title";
 
-import { ThemeProvider, createGlobalStyle } from "styled-components";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 import colors from "./components/theming/colors";
 
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: greenyellow;
   }
+`
+
+const ButtonRounded = styled(Button)`
+  border-radius: 15px;
+  padding: 1rem 2rem;
 `
 
 function App() {
@@ -20,6 +25,7 @@ function App() {
         <Button danger>Click Me!</Button>
         <Button ghost>Ghost Me!</Button>
         <Button danger ghost>Ghost Me!</Button>
+        <ButtonRounded danger ghost>Rounded</ButtonRounded>
       </ThemeProvider>
     </>
   );
