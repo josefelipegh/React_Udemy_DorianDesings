ff662336892d6118019cdc1ff9ce054e3f15de4e
Jose Felipe Gonzalez <josefelipe25abril@gmail.com>
2022-03-14 15:54:54 -0300

    useState con objetos

--- 11-hooks/src/App.jsx
+++ 11-hooks/src/App.jsx
@@ -1,34 +1,36 @@
 import React, { useState } from 'react';
+import Form from './components/Form';
 
 const App = () => {
 
-  const [number, setNumber] = useState(10);
-  const [text, setText] = useState('')
+  // const [number, setNumber] = useState(10);
+  // const [text, setText] = useState('')
 
-  const handleIncrement = () => {
-    setNumber(number + 1)
-  }
+  // const handleIncrement = () => {
+  //   setNumber(number + 1)
+  // }
 
-  const handleDecrement = () => {
-    setNumber(number - 1)
-  }
+  // const handleDecrement = () => {
+  //   setNumber(number - 1)
+  // }
 
-  const handleReset = () => {
-    setNumber(0)
-  }
+  // const handleReset = () => {
+  //   setNumber(0)
+  // }
 
-  const handleInput = (e) => {
-    setText(e.target.value)
-  }
+  // const handleInput = (e) => {
+  //   setText(e.target.value)
+  // }
 
   return (
     <>
-      <h1>Numbe: { number }</h1>
+      {/* <h1>Numbe: { number }</h1>
       <button onClick={handleIncrement}>Increment</button>
       <button onClick={handleDecrement}>Decrement</button>
       <button onClick={handleReset}>Reset</button>
       <p>Contenido del input {text}</p>
-      <input type="text" onChange={handleInput} />
+      <input type="text" onChange={handleInput} /> */}
+      <Form />
     </>
   );
 }
